4285c9b54e4bb54580cf6a196b758afb23e7c832
tomvanmele <tom.v.mele@gmail.com>
2024-01-23 23:18:10 +0100

    almost working donations

--- src/views/Contribute.vue
+++ src/views/Contribute.vue
@@ -155,70 +155,102 @@
                     <v-col cols="12" md="6" class="mx-auto">
                         <v-card class="w-100 pa-8" flat>
                             <v-row>
-                                <v-col class="d-flex justify-center">
+                                <v-col class="d-flex flex-column justify-center">
                                     <v-btn-toggle
-                                        v-model="toggle_exclusive"
+                                        v-model="donationFrequencyModel"
                                         rounded="xl"
                                         elevation="0"
-                                        class="w-100 bg-blue-grey-lighten-5 pa-1"
+                                        class="w-100 bg-blue-grey-lighten-5 pa-1 my-4"
                                     >
                                         <v-btn
                                             :class="
-                                                'w-50 ' + (toggle_exclusive == 0 ? 'bg-secondary' : 'bg-transparent')
+                                                'w-50 ' +
+                                                (donationFrequencyModel == 0 ? 'bg-secondary' : 'bg-transparent')
                                             "
                                             rounded="xl"
+                                            :ripple="false"
                                         >
                                             Monthly
                                         </v-btn>
                                         <v-btn
                                             :class="
-                                                'w-50 ' + (toggle_exclusive == 1 ? 'bg-secondary' : 'bg-transparent')
+                                                'w-50 ' +
+                                                (donationFrequencyModel == 1 ? 'bg-secondary' : 'bg-transparent')
                                             "
                                             rounded="xl"
+                                            :ripple="false"
                                         >
                                             One-time
                                         </v-btn>
                                     </v-btn-toggle>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col class="text-center">
-                                    <v-item-group selected-class="bg-secondary" v-model="model">
-                                        <v-item
-                                            v-for="(item, i) in donation.monthly"
-                                            :key="i"
-                                            :value="i"
-                                            v-slot="{ isSelected, selectedClass, toggle }"
-                                        >
-                                            <v-btn
-                                                :variant="isSelected ? 'flat' : 'outlined'"
-                                                :class="['ma-2', selectedClass]"
-                                                rounded="lg"
-                                                min-width="100px"
-                                                :color="isSelected ? 'primary' : 'secondary-lighten-2'"
-                                                @click="customToggle(toggle, i)"
+                                    <v-window v-model="donationFrequencyModel">
+                                        <v-window-item>
+                                            <v-item-group
+                                                class="text-center my-4"
+                                                selected-class="bg-secondary"
+                                                v-model="donationMonthlyAmountModel"
                                             >
-                                                &euro; {{ item.amount }}
-                                            </v-btn>
-                                        </v-item>
-                                    </v-item-group>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col>
-                                    <v-item-group v-model="model">
-                                        <v-item v-for="(item, i) in donation.monthly" v-slot="{ isSelected }" :key="i">
-                                            <div v-if="isSelected">
-                                                <p class="text-h6 text-secondary">{{ item.member }} member</p>
-                                                <p class="text-secondary">{{ item.rights }}</p>
-                                            </div>
-                                        </v-item>
-                                    </v-item-group>
-                                </v-col>
-                            </v-row>
-                            <v-row class="mt-8">
-                                <v-col>
-                                    <v-btn flat color="primary" block rounded="lg">Donate</v-btn>
+                                                <v-item
+                                                    v-for="(item, i) in donationMonthly"
+                                                    :key="i"
+                                                    :value="i"
+                                                    v-slot="{ isSelected, selectedClass, toggle }"
+                                                >
+                                                    <v-btn
+                                                        :variant="isSelected ? 'flat' : 'outlined'"
+                                                        :class="['ma-2', selectedClass]"
+                                                        rounded="lg"
+                                                        min-width="100px"
+                                                        :color="isSelected ? 'primary' : 'secondary-lighten-2'"
+                                                        @click="donationMonthlyAmountToggle(toggle, i)"
+                                                    >
+                                                        &euro; {{ item.amount }}
+                                                    </v-btn>
+                                                </v-item>
+                                            </v-item-group>
+                                            <v-item-group v-model="donationMonthlyAmountModel" class="my-4">
+                                                <v-item
+                                                    v-for="(item, i) in donationMonthly"
+                                                    v-slot="{ isSelected }"
+                                                    :key="i"
+                                                >
+                                                    <div v-if="isSelected">
+                                                        <p class="text-h6 text-secondary">
+                                                            Become a {{ item.member }} member
+                                                        </p>
+                                                        <p class="text-secondary">{{ item.rights }}</p>
+                                                    </div>
+                                                </v-item>
+                                            </v-item-group>
+                                            <v-btn flat color="primary" block rounded="lg" class="my-8">Donate</v-btn>
+                                        </v-window-item>
+                                        <v-window-item>
+                                            <v-item-group
+                                                class="text-center my-4"
+                                                selected-class="bg-secondary"
+                                                v-model="donationOnceAmountModel"
+                                            >
+                                                <v-item
+                                                    v-for="(item, i) in donationOnce"
+                                                    :key="i"
+                                                    :value="i"
+                                                    v-slot="{ isSelected, selectedClass, toggle }"
+                                                >
+                                                    <v-btn
+                                                        :variant="isSelected ? 'flat' : 'outlined'"
+                                                        :class="['ma-2', selectedClass]"
+                                                        rounded="lg"
+                                                        min-width="100px"
+                                                        :color="isSelected ? 'primary' : 'secondary-lighten-2'"
+                                                        @click="donationOnceAmountToggle(toggle, i)"
+                                                    >
+                                                        &euro; {{ item.amount }}
+                                                    </v-btn>
+                                                </v-item>
+                                            </v-item-group>
+                                            <v-btn flat color="primary" block rounded="lg" class="my-8">Donate</v-btn>
+                                        </v-window-item>
+                                    </v-window>
                                 </v-col>
                             </v-row>
                         </v-card>
@@ -248,44 +280,63 @@ export default {
         ContentSection,
     },
     methods: {
-        customToggle(toggle, i) {
+        donationMonthlyAmountToggle(toggle, i) {
             toggle();
-            this.model = i;
+            this.donationMonthlyAmountModel = i;
+        },
+        donationOnceAmountToggle(toggle, i) {
+            toggle();
+            this.donationOnceAmountModel = i;
         },
     },
     data: () => ({
-        model: 2,
-        donation: {
-            monthly: [
-                {
-                    amount: 5,
-                    member: "Bronze",
-                    rights: "Bronze member bagde.",
-                },
-                {
-                    amount: 10,
-                    member: "Silver",
-                    rights: "Silver member bagde.",
-                },
-                {
-                    amount: 25,
-                    member: "Gold",
-                    rights: "Name on the website.",
-                },
-                {
-                    amount: 50,
-                    member: "Platinum",
-                    rights: "Link on the website.",
-                },
-                {
-                    amount: 100,
-                    member: "Diamond",
-                    rights: "Link on the website.",
-                },
-            ],
-            once: [25, 50, 100, 250, 500],
-        },
-        toggle_exclusive: 0,
+        donationFrequencyModel: 0,
+        donationMonthlyAmountModel: 2,
+        donationOnceAmountModel: 0,
+        donationMonthly: [
+            {
+                amount: 5,
+                member: "Bronze",
+                rights: "Get a Bronze member bagde.",
+            },
+            {
+                amount: 10,
+                member: "Silver",
+                rights: "Get a Silver member bagde.",
+            },
+            {
+                amount: 25,
+                member: "Gold",
+                rights: "Get your name on the website.",
+            },
+            {
+                amount: 50,
+                member: "Platinum",
+                rights: "Get your link on the website.",
+            },
+            {
+                amount: 100,
+                member: "Diamond",
+                rights: "Get your link on the website.",
+            },
+        ],
+        donationOnce: [
+            {
+                amount: 25,
+            },
+            {
+                amount: 50,
+            },
+            {
+                amount: 100,
+            },
+            {
+                amount: 250,
+            },
+            {
+                amount: 500,
+            },
+        ],
         images: {
             develpment: compas_development_jpg,
             documentation: compas_documentation_png,
