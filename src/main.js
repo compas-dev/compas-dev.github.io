17658d68ba51f6f068c86127744f05c44e5b4ae5
tomvanmele <tom.v.mele@gmail.com>
2024-01-24 15:21:12 +0100

    proper external links

--- src/main.js
+++ src/main.js
@@ -13,6 +13,18 @@ import { createApp } from "vue";
 // Plugins
 import { registerPlugins } from "@/plugins";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// import { faLinkedin, faYoutube, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faArrowUpRightFromSquare } from "@fortawesome/pro-regular-svg-icons/faArrowUpRightFromSquare";
+
+/* add icons to the library */
+library.add(faArrowUpRightFromSquare);
+
 // Store
 import store from "./stores";
 
@@ -21,4 +33,5 @@ const app = createApp(App);
 registerPlugins(app);
 
 app.use(store);
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
