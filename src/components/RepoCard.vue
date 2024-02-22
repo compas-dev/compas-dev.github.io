7fb57d1e3eb68bde79aaf69900314c81ebab4def
tomvanmele <tom.v.mele@gmail.com>
2024-02-22 18:25:30 +0100

    use the correct organization

--- src/components/RepoCard.vue
+++ src/components/RepoCard.vue
@@ -1,37 +1,30 @@
 <template>
-    <v-card
-        flat
-        rounded="0"
-        :border="true"
-        :class="(smAndDown ? 'mb-0' : 'mb-8') + ' flex-grow-1'"
-        :href="`https://compas.dev/${repo.name}`"
-        target="_blank"
-    >
-        <div v-if="repo.image" class="img-fix-container">
-            <v-img :src="repo.image" class="img-fix" cover></v-img>
-        </div>
-        <div v-else class="img-fix-container">
-            <v-responsive max-width="1600px" max-height="1000px" :aspect-ratio="16 / 10">
-                <div class="bg-white w-100 h-100"></div>
-            </v-responsive>
-        </div>
-        <v-card-title class="mt-8">{{ repo.name }}</v-card-title>
-        <v-card-text>
-            <p>{{ repo.description }}</p>
-            <v-chip-group class="my-4">
-                <v-chip
-                    v-for="keyword in repo.keywords"
-                    :key="keyword"
-                    size="small"
-                    color="secondary"
-                    variant="tonal"
-                    label
-                >
-                    {{ keyword }}
-                </v-chip>
-            </v-chip-group>
-        </v-card-text>
-    </v-card>
+  <v-card
+    flat
+    rounded="0"
+    :border="true"
+    :class="(smAndDown ? 'mb-0' : 'mb-8') + ' flex-grow-1'"
+    :href="`https://${repo.organization}/${repo.name}`"
+    target="_blank"
+  >
+    <div v-if="repo.image" class="img-fix-container">
+      <v-img :src="repo.image" class="img-fix" cover></v-img>
+    </div>
+    <div v-else class="img-fix-container">
+      <v-responsive max-width="1600px" max-height="1000px" :aspect-ratio="16 / 10">
+        <div class="bg-white w-100 h-100"></div>
+      </v-responsive>
+    </div>
+    <v-card-title class="mt-8">{{ repo.name }}</v-card-title>
+    <v-card-text>
+      <p>{{ repo.description }}</p>
+      <v-chip-group class="my-4">
+        <v-chip v-for="keyword in repo.keywords" :key="keyword" size="small" color="secondary" variant="tonal" label>
+          {{ keyword }}
+        </v-chip>
+      </v-chip-group>
+    </v-card-text>
+  </v-card>
 </template>
 
 <style></style>
@@ -40,16 +33,16 @@
 import { useDisplay } from "vuetify";
 
 export default {
-    setup() {
-        const { smAndDown, mdAndDown, smAndUp, mdAndUp, platform, mobile } = useDisplay();
-        return { smAndDown, mdAndDown, smAndUp, mdAndUp, platform, mobile };
+  setup() {
+    const { smAndDown, mdAndDown, smAndUp, mdAndUp, platform, mobile } = useDisplay();
+    return { smAndDown, mdAndDown, smAndUp, mdAndUp, platform, mobile };
+  },
+  name: "RepoCard",
+  props: {
+    repo: {
+      type: Object,
+      default: () => ({}),
     },
-    name: "RepoCard",
-    props: {
-        repo: {
-            type: Object,
-            default: () => ({}),
-        },
-    },
+  },
 };
 </script>
